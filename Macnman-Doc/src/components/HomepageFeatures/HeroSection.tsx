b0e16f374f57023da81713ac8df48cb30ad9466b
Ram Gulave <gulaveram2@gmail.com>
2025-08-12 17:54:49 +0530

    mobile view

--- Macnman-Doc/src/components/HomepageFeatures/HeroSection.tsx
+++ Macnman-Doc/src/components/HomepageFeatures/HeroSection.tsx
@@ -1,71 +1,3 @@
-// import React from 'react';
-// import styles from '../../css/HeroSection.module.css';
-
-// const SupportSection: React.FC = () => {
-//     return (
-//         <div className={styles.supportContainer}>
-//             {/* Left Content */}
-//             <div className={styles.leftContent}>
-//                 <h2 className={styles.heading}>
-//                     Find Setup Guides, Technical Specs, &<br />
-//                     Troubleshooting Tips All in One Place
-//                 </h2>
-//             </div>
-
-//             <div className={styles.rightContent}>
-//                 {/* Item 1 */}
-//                 <div className={styles.item}>
-//                     <img
-//                         src="/docs/img/HeroSection/circle-girl.svg"
-//                         alt="Specialist"
-//                         style={{ 
-//                             width: '42px', 
-//                             height: '49px', 
-//                             borderRadius: '10%',
-//                             marginTop: '-1px',
-//                             verticalAlign: 'middle',
-//                         }}
-//                         className={styles.avatar}
-//                     />
-//                     <div>
-//                         <p className={styles.description}>Designing Your Wireless Solution?</p>
-//                         <a href="#" className={styles.link}>
-//                             Call a Specialist
-//                         </a>
-//                     </div>
-//                 </div>
-
-//                 {/* Item 2 - Replacing SVG with Image */}
-//                 <div className={styles.item}>
-//                     <img
-//                         src="/docs/img/HeroSection/shopping-bag 2.svg"
-//                         alt="Explore"
-//                         style={{ 
-//                             width: '34px', 
-//                             height: '40px',
-//                             borderRadius: '25%',
-//                             marginTop : '4px',
-//                             marginLeft : '2px',
-//                             marginRight: '6px',
-//                             verticalAlign: 'middle',
-//                         }}
-//                         className={styles.avatar}
-//                     />
-//                     <div style={{}}>
-//                         <p className={styles.description}>Discover complete lineup</p>
-//                         <a href="#" className={styles.link}>
-//                             Explore Our Store
-//                         </a>
-//                     </div>
-//                 </div>
-//             </div>
-
-//         </div>
-//     );
-// };
-
-// export default SupportSection;
-
 
 import React from 'react';
 import styles from '../../css/HeroSection.module.css';
@@ -102,7 +34,7 @@ const SupportSection: React.FC = () => {
                     <img
                         src="/docs/img/HeroSection/shopping-bag 2.svg"
                         alt="Explore"
-                        className={styles.avatar}
+                        className={styles.avatarNew}
                         style={{ width: '34px', height: '40px' }}
                     />
                     <div>
