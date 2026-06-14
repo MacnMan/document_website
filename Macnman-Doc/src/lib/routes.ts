3780e65617be78aee73a3e9ab34d9eff8bab8b50
Ram Gulave <balajigulve5@gmail.com>
2026-06-14 15:42:02 +0530

    Product Landing page

---
+++ Macnman-Doc/src/lib/routes.ts
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  analog_to_lorawan_converter_macsync_std: "/product/lorawan/dataloggers/macsync-lx-1",
+  lorawan_remote_automation_controller_macset_x_one: "/product/lorawan/controllers/1-macset-lx-one",
+  lorawan_ultrasonic_sensor_macray_lufour: "/product/lorawan/sensors/2-level-sensors/1-macray-lus-x-one",
+  outdoor_lorawan_gateway_maclink_gen_two: "/product/lorawan/gateways/1-maclink-gen-two-x",
+  indoor_lorawan_gateway_setu_x_one: "/product/lorawan/gateways/2-setu-x-one",
+  ble_module_mac52810: "/product/ble/ble-trackers/1-tag-x-three",
+  HOME: "/",
+};
--- Macnman-Doc/src/pages/index.tsx
+++ Macnman-Doc/src/pages/index.tsx
@@ -1,56 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
-import styles from './index.module.css';
+import React from 'react';
 import Layout from '@theme/Layout';
-
-import FilterBar from '../components/HomepageFeatures/FilterBar';
-import FilterCategory from '../components/HomepageFeatures/FilterCategories';
-import { FilterContext } from '../components/HomepageFeatures/FilterContext';
-import HeroSection from '../components/HomepageFeatures/HeroSection';
+import ProductPage from '../components/HomepageFeatures/ProductPage';
 
 export default function Home() {
-  const [filters, setFilters] = useState({
-    Filter: 'All',
-    Technology: 'All',
-    Catagory: 'All',
-  });
-
-  const heroRef = useRef(null);
-  const [isSticky, setIsSticky] = useState(false);
-
-  useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        setIsSticky(!entry.isIntersecting);
-      },
-      { threshold: 0 }
-    );
-
-    if (heroRef.current) {
-      observer.observe(heroRef.current);
-    }
-
-    return () => {
-      if (heroRef.current) {
-        observer.unobserve(heroRef.current);
-      }
-    };
-  }, []);
-
   return (
-    <Layout title="Documentation">
-      <div ref={heroRef} className={styles.heroWrapper}>
-        <HeroSection />
-      </div>
-      <FilterContext.Provider value={{ filters, setFilters }}>
-        <div className={styles.pageWrapper}>
-          <div className={`${styles.sidebar} ${isSticky ? styles.stickySidebar : ''}`}>
-            <FilterBar />
-          </div>
-          <div className={styles.content}>
-            <FilterCategory />
-          </div>
-        </div>
-      </FilterContext.Provider>
+    <Layout title="Products">
+      <ProductPage />
     </Layout>
   );
 }
+
